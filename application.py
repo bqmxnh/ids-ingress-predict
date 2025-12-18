484adeab02f9e9ebf275cd8b1c5886df1a254fcb
Quoc Minh Bui <bqmxnh@gmail.com>
2025-12-18 13:05:19 +0700

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import eventlet
 eventlet.monkey_patch()
+from eventlet.semaphore import Semaphore
 import logging
 import json
 import threading
@@ -62,7 +63,7 @@ HONEYPOT_URL = "http://honeypot.qmuit.id.vn/receive_attack"
 EMAIL_LAMBDA_URL=mail_url
 ATTACK_BUFFER = deque() 
 BATCH_TIMEOUT = 60
-LOCK = threading.Lock()
+LOCK = Semaphore()
 last_attack_time = None
 
 logger.info("=" * 60)
@@ -264,7 +265,7 @@ def batch_alert_worker():
     global last_attack_time
     
     while True:
-        time.sleep(1)
+        eventlet.sleep(1)
         batch_data = None
         with LOCK:
             if not ATTACK_BUFFER:
@@ -278,11 +279,10 @@ def batch_alert_worker():
                 last_attack_time = None
                 
         if batch_data:
-            threading.Thread(
-                target=send_email_alert,
-                args=(batch_data,),
-                daemon=True
-            ).start()
+            socketio.start_background_task(
+                send_email_alert,
+                batch_data
+            )
 
 def send_email_alert(batch_data):  # ✅ NHẬN batch_data từ args
     """Send batch email alert after timeout"""
@@ -361,7 +361,7 @@ def send_email_alert(batch_data):  # ✅ NHẬN batch_data từ args
         logger.error(f"[EMAIL ERROR] {str(e)}")
 
 # Start batch worker thread
-threading.Thread(target=batch_alert_worker, daemon=True).start()
+socketio.start_background_task(batch_alert_worker)
 #############
 
 try:
@@ -371,7 +371,7 @@ except:
     table = None
 
 flow_results = []
-flow_lock = threading.Lock()
+flow_lock = Semaphore()
 
 def safe(v):
     try:
@@ -450,7 +450,8 @@ def log_ingest(result):
         except Exception as e:
             logging.error(f"DynamoDB ingest error: {e}")
 
-    threading.Thread(target=worker, daemon=True).start()
+    socketio.start_background_task(worker)
+
 
 
 
@@ -489,7 +490,8 @@ def log_feedback(result):
         except Exception as e:
             logging.error(f"DynamoDB update error: {e}")
 
-    threading.Thread(target=worker, daemon=True).start()
+    socketio.start_background_task(worker)
+
 
 
 
@@ -518,12 +520,12 @@ def process_flow(p):
     }
 
     #QuanTC add:
-    threading.Thread(
-        target=redirect_to_honeypot,
-        args=(p, label, conf),
-        daemon=True
-    ).start()
-    #MinhBQ add: chạy hàm redirect_to_honeypot trong thread riêng để không làm chậm quá trình ingest
+    socketio.start_background_task(
+        redirect_to_honeypot,
+        p, label, conf
+    )
+
+    #MinhBQ add: background thread
     ####
 
     with flow_lock:
@@ -542,14 +544,10 @@ def ingest_flow():
         p = request.get_json(force=True)
         if "batch" in p:
             for x in p["batch"]:
-                threading.Thread(
-                    target=process_flow,
-                    args=(x,),
-                    daemon=True
-                ).start()
+                socketio.start_background_task(process_flow, x)
             return jsonify({"status": "accepted", "count": len(p["batch"])}), 202
 
-        threading.Thread(target=process_flow, args=(p,), daemon=True).start()
+        socketio.start_background_task(process_flow, p)
         return jsonify({"status": "accepted"}), 202
     except Exception as e:
         logging.error(f"Ingest error: {e}")
